cca9c111518a045a55c2b98ece737ef9a924f00b
ChristyTropila <tropila04@gmail.com>
2020-09-14 22:48:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Your code here
 
-let dodger= document.geteElementById("dodger");
+let dodger= document.getElementById("dodger");
 
 function moveDodgerLeft() {
     let leftNumbers = dodger.style.left.replace("px", "");
@@ -21,7 +21,7 @@ function moveDodgerLeft() {
     let leftNumbers = dodger.style.left.replace("px", "");
     let left = parseInt(leftNumbers, 10);
    
-    if (left > 0) {
+    if (left < 360) {
       dodger.style.left = `${left + 1}px`;
     }
   }
